b70aa13fe83c8be8a93b66fc074ec539249905b3
KexuanZhang <kex.zhang@mail.utoronto.ca>
2023-09-10 15:27:27 -0400

    s

--- PUMC_Backend/settings.py
+++ PUMC_Backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
+    "pre_questionaire",
+    "post_questionaire",
 ]
 
 MIDDLEWARE = [
--- pre_questionaire/models.py
+++ pre_questionaire/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from users import User
 
 # Create your models here.
+class Pre_Questionaire(models.Model):
+    user = models.ForeignKey("User", verbose_name=_("user"), on_delete=models.CASCADE)
+    
